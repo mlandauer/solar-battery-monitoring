0ac15f49314ca27ce3c3ac252154f625758c4db8
Matthew Landauer <mlandauer@gmail.com>
2020-02-12 08:55:10 +1100

    Move high-level read operations to their own file

---
+++ pkg/pli/read.go
@@ -0,0 +1,101 @@
+package pli
+
+import "errors"
+
+// Methods for reading values (at a high level) from the PLI
+
+// Potentially useful addresses to read from in RAM
+//
+// 0 - Software version number.The following applies (subject to change without notice):Version      0-127 = PL20Version  128-191 = PL40Version  192-210 = PL60Version  215-255 = PL80
+// batv - 50 - Battery voltage in 0.1V steps scaled relative to 12V.eg. 128=12.8V, for 24V system 128*2=25.6V, for 48V system128*4=51.2V
+// solv - 53  - solar voltage msb
+// volt - 93 - msn= Prog number (0-4), lsn=system voltage (0-4)System voltage... 0=12V, 1=24V, 2=32V, 3=36V, 4=48VEg. 00110001 = 24V system running Prog 3.
+// bcap - 94 - battery capacity in 20/100 Ah chunks
+// bminl - 124 - lower byte of battery min voltage scaled to 12V
+// bmaxl - 125 - lower byte of battery max voltage scaled to 12V
+// dtemp - 180 - current external temperature
+// dsoc- 181 - SOC (day data state of charge)
+// ciahl - 188 - Internal charge ah low byte
+// ciahh - 189 - Internal charge ah high byte
+// ceahl - 193 - External charge ah low byte
+// ceahh - 194 - External charge ah high byte
+// liahl - 198 - Internal load ah low byte
+// liahh - 199 - Internal load ah high byte
+// leahl - 203 - External load ah low byte
+// leahh - 204 - External load ah high byte
+// cext - 205 - external charge input (NOTE: First read ‘extf’ to check validity andscaling)
+// lext - 206 - external load input (NOTE: First read ‘extf’ to check validity andscaling)
+// extf - 207 - external flag and scale fileBit 3, Enable of LEXT.Bit 2, Enable for CEXTBit 1, 1=1A/step for LEXT (times 10), 0=0.1A/step for LEXTBit 0, 1=1A/step for CEXT (times 10), 0=0.1A/step for CEXT
+// vext - 208 - external voltage reading 0-255 volt 1V steps
+// cint - 213 - Internal (solar) charge current:0.1A steps for PL20 (eg. 10=1.0 Amp solar charge)0.2A steps for PL40 (eg. 10=2.0 Amps solar charge)0.4A steps for PL60 (eg. 10=4.0 Amps solar charge)
+// lint - 217 - Internal LOAD- current:0.1A steps for PL20/PL40 (eg. 10=1.0A), 0.2A steps for PL60 (eg.10=2.0A)
+// batvl - 220 - battery voltage lsb
+// vbat - 221 - battery voltage msb
+// solvl - 232 - solar voltage lsb
+// vsol - 233 - solar voltage msb
+
+// Reading the solar voltage is complicated because the charging needs to be stopped and the
+// display activated to get an accurate reading
+
+func (pli *PLI) SoftwareVersion() (string, byte, error) {
+	value, err := pli.ReadRAM(0)
+	if err != nil {
+		return "", value, err
+	}
+	if value < 128 {
+		return "PL20", value, nil
+	} else if value < 192 {
+		return "PL40", value, nil
+	} else if value < 211 {
+		return "PL60", value, nil
+	} else {
+		return "PL80", value, nil
+	}
+}
+
+// BatterCapacity returns the capacity of the battery measured in Ah
+func (pli *PLI) BatteryCapacity() (int, error) {
+	b, err := pli.ReadRAM(94)
+	if err != nil {
+		return 0, err
+	}
+	// Battery capacity setting 20A/100A per step - 20A steps until 1000Ah, 100Ah steps >1000Ah
+	var value int
+	value = int(b) * 20
+	if value > 1000 {
+		value = 1000 + (value-1000)/20*100
+	}
+	return value, nil
+}
+
+// Gets the overall PL program number and the system voltage
+func (pli *PLI) volt() (prog int, voltage int, err error) {
+	v, err := pli.ReadRAM(93)
+	progByte, voltByte := extractNibbles(v)
+	if progByte > 4 {
+		err = errors.New("Expected program number to be in the range 0-4")
+		return
+	}
+	prog = int(progByte)
+	switch voltByte {
+	case 0:
+		voltage = 12
+	case 1:
+		voltage = 24
+	case 2:
+		voltage = 32
+	case 3:
+		voltage = 36
+	case 4:
+		voltage = 48
+	default:
+		err = errors.New("Unexpected voltage value returned by PLI")
+	}
+	return
+}
+
+func (pli *PLI) BatteryVoltage() (float32, error) {
+	b, err := pli.ReadRAM(50)
+	value := float32(b) * 0.1 * float32(pli.Voltage) / 12
+	return value, err
+}
